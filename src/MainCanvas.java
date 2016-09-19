043356bd47cbc74b86f683fcd18900927fa2325c
Kevin Traw <ktraw2@gmail.com>
2016-09-18 20:41:53 -0700

    Implemented the 4 rules of the Game of Life

--- src/MainCanvas.java
+++ src/MainCanvas.java
@@ -3,6 +3,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.font.GraphicAttribute;
 import java.awt.image.BufferedImage;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -14,9 +15,10 @@ public class MainCanvas extends Canvas implements Runnable, KeyListener {
     private final int NUM_TEXT = 4;
 
     private Thread runThread = null;
-    private int gridSize = 10;
+    private int gridSize = 25;
     private int simulationSpeed = 1;
     private boolean started = false;
+    private boolean oneGenOnly = false;
     private ArrayList<Point> organismLocations = null; //the points of organisms
 
     public void update(Graphics g)
@@ -46,19 +48,28 @@ public class MainCanvas extends Canvas implements Runnable, KeyListener {
             runThread = new Thread(this);
             runThread.start();
         }
+
         //initialize a default location set for the organisms
         if (organismLocations == null)
         {
             organismLocations = new ArrayList<Point>();
-            organismLocations.add(new Point(5, 5));
-            organismLocations.add(new Point(4, 6));
-            organismLocations.add(new Point(4, 4));
-            organismLocations.add(new Point(3, 5));
+            organismLocations.add(new Point(10, 10));
+            organismLocations.add(new Point(10, 11));
+            organismLocations.add(new Point(10, 12));
+            organismLocations.add(new Point(10, 13));
+            organismLocations.add(new Point(10, 14));
+            organismLocations.add(new Point(12, 10));
+            organismLocations.add(new Point(12, 14));
+            organismLocations.add(new Point(14, 10));
+            organismLocations.add(new Point(14, 11));
+            organismLocations.add(new Point(14, 12));
+            organismLocations.add(new Point(14, 13));
+            organismLocations.add(new Point(14, 14));
         }
 
         //paint the main screen on the BufferedImage
         //draw text telling the user the application's controls
-        g.drawString("CONTROLS:", TEXT_X, TEXT_Y_OFFSET);
+        g.drawString("CONTROLS: SPACE to do 1 generation,", TEXT_X, TEXT_Y_OFFSET);
         g.drawString("CLICK on grid spots to add an organism, ENTER to start/stop,", TEXT_X, TEXT_Y_OFFSET * (NUM_TEXT / 2));
         g.drawString("RIGHT ARROW to increase simulation speed, LEFT ARROW to decrease simulation speed,", TEXT_X, TEXT_Y_OFFSET * (NUM_TEXT - 1));
         g.drawString("UP ARROW to increase grid size, DOWN ARROW to decrease grid size", TEXT_X, TEXT_Y_OFFSET * NUM_TEXT);
@@ -101,11 +112,95 @@ public class MainCanvas extends Canvas implements Runnable, KeyListener {
     {
         while (true)
         {
+            if (started)
+            {
+                if (oneGenOnly == true)
+                {
+                    started = false;
+                    oneGenOnly = false;
+                }
+
+                //System.out.println("Started simulation");
+                ArrayList<Point> newPoints = new ArrayList<Point>();
+                for (Point p : organismLocations) //apply rules 1, 2, and 3 to the live cells
+                {
+                    //System.out.println("rule 1,2,3 point p: " + p.x + ", " + p.y);
+                    ArrayList<Point> removedPoints = new ArrayList<Point>();
+                    int surroundingOrganisms = 0;
+//                    for (Point t : organismLocations)
+//                    {
+//                        System.out.println("rule 1,2,3 point t: " + t.x + ", " + t.y);
+//                        if (t.x == p.x || t.x == p.x - 1 || t.x == p.x + 1)
+//                            if (t.y == p.y || t.y == p.y - 1 || t.y == p.y + 1)
+//                                if (!(t.x == p.x && t.y == p.y))
+//                                    surroundingOrganisms++;
+//                    }
+                    if (organismLocations.contains(new Point(p.x - 1, p.y - 1)))
+                        surroundingOrganisms++;
+                    if (organismLocations.contains(new Point(p.x, p.y - 1)))
+                        surroundingOrganisms++;
+                    if (organismLocations.contains(new Point(p.x + 1, p.y - 1)))
+                        surroundingOrganisms++;
+                    if (organismLocations.contains(new Point(p.x - 1, p.y)))
+                        surroundingOrganisms++;
+                    if (organismLocations.contains(new Point(p.x + 1, p.y)))
+                        surroundingOrganisms++;
+                    if (organismLocations.contains(new Point(p.x - 1, p.y + 1)))
+                        surroundingOrganisms++;
+                    if (organismLocations.contains(new Point(p.x, p.y + 1)))
+                        surroundingOrganisms++;
+                    if (organismLocations.contains(new Point(p.x + 1, p.y + 1)))
+                        surroundingOrganisms++;
+                    if (surroundingOrganisms < 2)
+                    {
+                        removedPoints.add(p);
+                        //System.out.println("removed " + p.toString());
+                    }
+                    if (surroundingOrganisms == 2 || surroundingOrganisms == 3)
+                    {
+                        newPoints.add(p);
+                        //System.out.println("added " + p.toString());
+                    }
+                    if (surroundingOrganisms > 3)
+                    {
+                        removedPoints.add(p);
+                        //System.out.println("removed " + p.toString());
+                    }
+                }
+
+                //apply rule 4 to all dead cells
+                for (int x = 1; x <= gridSize; x++)
+                {
+                    for (int y = 1; y <= gridSize; y++) {
+                        //System.out.println("NEW LOOP: " + x + ", " + y);
+                        int surroundingOrganisms = 0;
+                        if (!(organismLocations.contains(new Point(x, y))))
+                        {
+                            for (Point t : organismLocations) {
+                                //System.out.println("rule 4 point t: " + t.x + ", " + t.y);
+                                //System.out.println("rule 4 point x,y: " + x + ", " + y);
+                                if ((t.x == x || t.x == x - 1 || t.x == x + 1) && (t.y == y || t.y == y - 1 || t.y == y + 1))
+                                    //System.out.println("rule 4 found point: " + t.x + ", " + t.y);
+                                    surroundingOrganisms++;
+                            }
+                        }
+                        //System.out.println("rule 4 organisms: " + surroundingOrganisms);
+                        if (surroundingOrganisms == 3) {
+                            newPoints.add(new Point(x, y));
+                            //System.out.println("added " + x + ", " + y);
+                        }
+                    }
+                }
+
+                organismLocations.removeAll(organismLocations);
+                organismLocations = new ArrayList<Point>();
+                organismLocations.addAll(newPoints);
+            }
             repaint();
             try
             {
                 Thread.currentThread();
-                Thread.sleep(100);
+                Thread.sleep(1000 / simulationSpeed);
             }
             catch (Exception e)
             {
@@ -117,17 +212,22 @@ public class MainCanvas extends Canvas implements Runnable, KeyListener {
     @Override
     public void keyTyped(KeyEvent e)
     {
-        System.out.println("kt");
     }
 
     @Override
     public void keyPressed(KeyEvent e)
     {
-        System.out.println("kp");
         switch (e.getKeyCode())
         {
+            case KeyEvent.VK_SPACE:
+                started = true;
+                oneGenOnly = true;
+                break;
             case KeyEvent.VK_ENTER:
-                started = !started;
+                if (started == true)
+                    started = false;
+                else
+                    started = true;
                 break;
             case KeyEvent.VK_LEFT:
                 if (simulationSpeed > 1)
@@ -149,6 +249,5 @@ public class MainCanvas extends Canvas implements Runnable, KeyListener {
     @Override
     public void keyReleased(KeyEvent e)
     {
-        System.out.println("kr");
     }
 }
